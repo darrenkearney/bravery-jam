5153e6c9de595d55f19dceae87d5984c9840a5c6
Ross Byrne <ross.byrne@outlook.ie>
2017-06-03 22:40:03 +0100

    finished commenting the 2 player orbit code

--- experiments/two-player-orbit-test/static/js/main.js
+++ experiments/two-player-orbit-test/static/js/main.js
@@ -1,3 +1,5 @@
+
+// create the game
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
 
 // variables
@@ -17,25 +19,31 @@ function preload() {
 } // preload()
 
 
-// creates objects when game starts
+// runs when game starts
 function create() {
 
+    // set background colour
     game.stage.backgroundColor = '#001255';
 
+    // add the planet to the game and anchor it
     planet = game.add.sprite(game.world.centerX, game.world.centerY, 'planet');
     planet.anchor.setTo(0.5, 0.5);
 
+    // add physics to the planet
     game.physics.arcade.enable(planet);
 
+    // add player one to the game
     player1 = game.add.sprite(400, 300, 'player');
-    player1.anchor.setTo(0.5);
-    player1.pivot.x = 240;
+    player1.anchor.setTo(0.5);      // anchor player
+    player1.pivot.x = 240;          // set the players pivot. (how far from it's position it is)
 
+    // add player two to the game
     player2 = game.add.sprite(400, 300, 'player');
-    player2.anchor.setTo(0.5);
-    player2.pivot.x = -240;
+    player2.anchor.setTo(0.5);      // anchor player
+    player2.pivot.x = -240;         // set the players pivot. (how far from it's position it is)
 
-    cursors = game.input.keyboard.createCursorKeys();
+    // sets up game to read key presses
+    //cursors = game.input.keyboard.createCursorKeys();
 
 } // create()
 
