cf8848b86446777bed5251bd41f730c74a2d7ce7
Darren Kearney <github@darrenk.net>
2017-06-03 22:58:10 +0100

    Added missile.png image to game assets, some edting to main for adding in assets 	modified:   assets/js/main.js 	new file:   assets/sprites/missile.png 	modified:   experiments/physics_test/static/js/main.js

--- experiments/physics_test/static/js/main.js
+++ experiments/physics_test/static/js/main.js
@@ -1,15 +1,15 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
 
+var ship;
+var orb;
+var cursors;
+
 function preload() {
 
     game.load.image('ship', 'assets/sprites/shmup-ship2.png');
     game.load.image('ball', 'assets/sprites/blue_ball.png');
 
 }
-
-var ship;
-var orb;
-var cursors;
 
 function create() {
 
