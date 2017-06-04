2d425a56eda88270bf24fa3b4b6428961a007a50
Darren Kearney <github@darrenk.net>
2017-06-04 11:39:06 +0100

    Refactored code and added assets. Seperated preload and create functions into init.js 	new file:   assets/js/controllers/init.js 	modified:   assets/js/main.js 	new file:   assets/sprites/planet.png 	new file:   assets/sprites/ship.png 	modified:   index.html

---
+++ assets/js/controllers/init.js
@@ -0,0 +1,53 @@
+function preloadAssets( view ) {
+    //console.log('Preloading...');
+
+    // load game sprites
+    game.load.image('logo', 'assets/sprites/phaser.png');
+    game.load.image('planet', 'assets/sprites/planet.png');
+    game.load.image('ship', 'assets/sprites/ship.png');
+    game.load.image('missile', 'assets/sprites/missile.png');
+
+    //console.log('Preloaded.');
+}
+
+function createGame( view ) {
+
+    // Global Create
+    
+    // set background colour
+    game.stage.backgroundColor = '#001255';
+
+    // add the planet to the game and anchor it
+    planet = game.add.sprite(game.world.centerX, game.world.centerY, 'planet');
+    planet.anchor.setTo(0.5, 0.5);
+
+    // add physics to the planet
+    game.physics.arcade.enable(planet);
+
+    // sets up game to read key presses
+    cursors = game.input.keyboard.createCursorKeys();
+
+    if (view === "battle") {
+
+    //console.log('Creating...');
+
+        // var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        //logo.anchor.setTo(0.5, 0.5);
+
+        // stop the planet from moving
+        planet.body.immovable = true;
+
+        // add player one to the game
+        player1 = game.add.sprite(400, 300, 'ship');
+        player1.anchor.setTo(0.5);      // anchor player
+        player1.pivot.x = 240;          // set the players pivot. (how far from it's position it is)
+
+        // add player two to the game
+        player2 = game.add.sprite(400, 300, 'ship');
+        player2.anchor.setTo(0.5);      // anchor player
+        player2.pivot.x = -240;         // set the players pivot. (how far from it's position it is)
+
+        console.log('Created.');
+
+    } // if view === battle
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+
 
     // create the game
     var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
@@ -18,53 +18,14 @@ window.onload = function () {
     // runs before the game starts
     function preload() {
 
-        //console.log('Preloading...');
-
-        // load game sprites
-        game.load.image('logo', 'assets/sprites/phaser.png');
-        game.load.image('planet', 'assets/sprites/planet.png');
-        game.load.image('ship', 'assets/sprites/ship.png');
-        game.load.image('missile', 'assets/sprites/missile.png');
-
-        //console.log('Preloaded.');
+        preloadAssets(view);
 
     } // preload()
 
     // runs when the game starts
     function create() {
 
-        //console.log('Creating...');
-
-       // var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        //logo.anchor.setTo(0.5, 0.5);
-
-        // set background colour
-        game.stage.backgroundColor = '#001255';
-
-        // add the planet to the game and anchor it
-        planet = game.add.sprite(game.world.centerX, game.world.centerY, 'planet');
-        planet.anchor.setTo(0.5, 0.5);
-
-        // add physics to the planet
-        game.physics.arcade.enable(planet);
-
-        // stop the planet from moving
-        planet.body.immovable = true;
-
-        // add player one to the game
-        player1 = game.add.sprite(400, 300, 'ship');
-        player1.anchor.setTo(0.5);      // anchor player
-        player1.pivot.x = 240;          // set the players pivot. (how far from it's position it is)
-
-        // add player two to the game
-        player2 = game.add.sprite(400, 300, 'ship');
-        player2.anchor.setTo(0.5);      // anchor player
-        player2.pivot.x = -240;         // set the players pivot. (how far from it's position it is)
-
-        // sets up game to read key presses
-        cursors = game.input.keyboard.createCursorKeys();
-
-        //console.log('Created.');
+        createGame(view);
         
     } // create()
 
@@ -110,4 +71,3 @@ window.onload = function () {
 
     } // checkForKeyPress()
 
-};
